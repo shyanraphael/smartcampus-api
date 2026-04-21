56c8fa0d97704ea1eba13bdb8da62bdeb5e040ac
Shyan Wijesekera <87508593+shyanraphael@users.noreply.github.com>
2026-04-21 11:23:40 +0530

    Fix comment formatting in DataStore class
    Corrected comment formatting in DataStore.java

--- src/main/java/com/smartcampus/model/DataStore.java
+++ src/main/java/com/smartcampus/model/DataStore.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-/**
+/*
     DataStore is a singleton class that acts as our in-memory database
     As we cannot use a real database, we store everything in HashMaps
 
