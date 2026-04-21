5c3d405c66ecf19b99d74d56c162c9ea29ddce4c
Shyan Wijesekera <87508593+shyanraphael@users.noreply.github.com>
2026-04-21 23:57:15 +0530

    Remove comments from LinkedResourceNotFoundExceptionMapper
    Removed detailed comments explaining the purpose of the LinkedResourceNotFoundExceptionMapper class.

--- src/main/java/com/smartcampus/exception/LinkedResourceNotFoundExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/LinkedResourceNotFoundExceptionMapper.java
@@ -7,19 +7,6 @@ import jakarta.ws.rs.ext.Provider;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
-    Maps LinkedResourceNotFoundException through the HTTP 422 Unprocessable Entity
-    Triggered when: POST /api/v1/sensors is called with a roomId that does not exist in the system
-    
-    WHY 422 AND NOT 404?
-        - 404 "Not Found" means the URL endpoint itself doesn't exist
-            eg: GET /api/v1/nonexistent -> 404 because that URL isn't real
-        - 422 "Unprocessable Entity" means the URL is valid and the JSON is valid, but the CONTENT inside the JSON is significantly wrong
-            eg: POST /api/v1/sensors with {"name":"Temp","roomId":999} -> 422 cause the roomId references to a  non-existent room
-    The request body references a roomId that doesn't exist.
-    The server understood what you sent — it just can't process it.
-    422 is far more accurate and informative to API consumers
- */
 @Provider
 public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
 
